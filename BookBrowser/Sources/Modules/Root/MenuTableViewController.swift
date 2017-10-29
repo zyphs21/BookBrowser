d9a2bc8a961b65fb12bead720e603181f58fbbeb
Hanson <hansenhs21@live.com>
2017-10-29 12:14:55 +0800

    code for the menuTableViewController

--- BookBrowser/Sources/Modules/Root/MenuTableViewController.swift
+++ BookBrowser/Sources/Modules/Root/MenuTableViewController.swift
@@ -9,45 +9,45 @@
 import UIKit
 
 class MenuTableViewController: UITableViewController {
-//    var menuItems = ["文学作品", "流行作品", "文化作品", "生活作品", "经管作品", "科技作品"]
-//    var currentItem = "文学作品"
-//    
-//    override func preferredStatusBarStyle() -> UIStatusBarStyle {
-//        return UIStatusBarStyle.LightContent
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
-//        //self.view.backgroundColor = UIColor(red:0.109, green:0.114, blue:0.128, alpha:1)
-//    }
-//    
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//    }
-//    
-//    
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return menuItems.count
-//    }
-//    
-//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier("MenuCell", forIndexPath: indexPath) as! MenuTableViewCell
-//        
-//        cell.menuLabel.text = menuItems[indexPath.row]
-//        cell.menuLabel.textColor = (menuItems[indexPath.row] == currentItem) ? UIColor.whiteColor() : UIColor.grayColor()
-//        cell.backgroundColor = UIColor.clearColor()
-//        
-//        return cell
-//    }
-//    
+    var menuItems = ["文学作品", "流行作品", "文化作品", "生活作品", "经管作品", "科技作品"]
+    var currentItem = "文学作品"
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.separatorStyle = .none
+        self.tableView.register(UITableViewCell.self)
+        //self.view.backgroundColor = UIColor(red:0.109, green:0.114, blue:0.128, alpha:1)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        cell.textLabel?.textColor = (menuItems[indexPath.row] == currentItem) ? UIColor.white : UIColor.gray
+        cell.backgroundColor = UIColor.clear
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currentItem = menuItems[indexPath.row]
+    }
+    
 //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//        
+//
 //        if(segue.identifier == "backToFirstViewSegue"){
 //            let menuTableViewController = segue.sourceViewController as! MenuTableViewController
 //            if let selectedRow = menuTableViewController.tableView.indexPathForSelectedRow?.row {
@@ -55,7 +55,4 @@ class MenuTableViewController: UITableViewController {
 //            }
 //        }
 //    }
-    
-    
-    
 }
